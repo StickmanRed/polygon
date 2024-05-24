8ade28e7763df2adafdc7b09aa727a8abec448ba
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-05-24 13:33:34 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,17 @@
-const canvas = document.createElement("canvas");
+const game = {
+  canvas: document.createElement("canvas");
+  setupCanvas() {
+    document.getElementById("canvasContainer").appendChild(canvas);
+    canvas.id = "gameCanvas";
+    canvas.style.width = "100%";
+    canvas.style.height = "100%";
+    canvas.width  = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+    this.ctx = canvas.getContext("2d");
+  }
+}
+
+/*const canvas = document.createElement("canvas");
 document.getElementById("canvasContainer").appendChild(canvas);
 canvas.id = "gameCanvas"
 canvas.style.width = "100%";
@@ -6,6 +19,8 @@ canvas.style.height = "100%";
 canvas.width  = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");*/
+game.setupCanvas;
+ctx = game.ctx;
 ctx.fillStyle = "red";
 ctx.fillRect(0, 0, 150, 75);

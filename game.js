a6fab54da6e5bb79ff273037519f799f0f7ee130
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-05-24 19:07:06 -0700

    Update game.js

--- game.js
+++ game.js
@@ -11,6 +11,7 @@ const game = {
     this.ctx = this.canvas.getContext("2d");
   },
   clearCanvas() {
+    this.ctx.setTransform(0, 0, 0, 0, 0, 0);
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
   },
 

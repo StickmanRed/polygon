582695414c4074dbe822aeb71e387fd324e5b497
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-07-15 20:43:02 -0700

    Sprite class to sprite.js + Point-in-sprite finalization (#1)
    * Remove sprite code
    
    * Add sprite code
    
    * Update game.js
    
    * Update game.js
    
    * Update game.js
    
    * Modules use strict mode
    
    * Update game.js
    
    * Update game.js
    
    * Update sprite.js
    
    * Update README.md
    
    * Update game.js
    
    * Update sprite.js
    
    * Update game.js
    
    * Clean up point-in-sprite tests
    
    * Update README.md

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Right now, I'm making a sprite system for a game. Feel free to check it out!
 
 ### TODO
-- [ ] Finish point-in-matrix detection for mouse
-- [ ] Move sprite class to sprite.js
+- [x] Finish point-in-matrix detection for mouse
+- [x] Move sprite class to sprite.js
--- game.js
+++ game.js
@@ -1,15 +1,4 @@
-// Huge thanks to whoever wrote this! https://stackoverflow.com/a/60248778
-function transformMatrix(dx, dy, scale, rotate) {
-  const xAX = Math.cos(rotate) * scale;
-  const xAY = Math.sin(rotate) * scale;
-  return [xAX, xAY, -xAY, xAX, dx, dy];
-}
-
-/* The transformation matrix for scaleX, skewY, skewX, scaleY, shiftX, shiftY:
- * [ scaleX skewX  shiftX ]
- * [ skewY  scaleY shiftY ]
- * [ 0      0      1      ]
- */
+import Sprite from "./sprite.js";
 
 const game = {
   canvas: document.createElement("canvas"),
@@ -50,101 +39,33 @@ const game = {
     sprite.ctx = this.ctx;
   }
 }
-class Sprite {
-  constructor(x=0, y=0, scale=1, rotation=0, shown=true) {
-    this.x = x;
-    this.y = y;
-    this.scale = scale;
-    this.rotation = (rotation + 180) % 360 - 180; // degrees are measured as in Scratch; up is 0, right is 90, down is -180
-    this.shown = shown;
-    this.ctx = null;
-
-    let defaultShape = new Path2D();
-    defaultShape.rect(0,0,40,40);
-    
-    this.shapes = [[defaultShape, true, false]];
-    this.costume = 0;
-  }
-
-  getTransform(x=this.x, y=this.y, scale=this.scale, rotation=this.rotation) {
-    return transformMatrix(x, y, scale, rotation * Math.PI / 180);
-  }
-
-  pointInSprite(x=0, y=0) {
-    // For this, probably just rotate the point and check if the transformed point is in the transformed path
-    const transformValues = this.getTransform();
-    const matrix = math.matrix([[transformValues[0], transformValues[2], transformValues[4]], [transformValues[1], transformValues[3], transformValues[5]], [0, 0, 1]]);
-    const inverseMatrix = math.inv(matrix);
-    let newX = inverseMatrix.get([0,0]) * x + inverseMatrix.get([0,1]) * y + inverseMatrix.get([0,2]);
-    let newY = inverseMatrix.get([1,0]) * x + inverseMatrix.get([1,1]) * y + inverseMatrix.get([1,2]);
-    text.textContent = `x: ${Math.round(newX)}, y: ${Math.round(newY)}`;
-    for (const shape of this.shapes) {
-      if ((this.ctx.isPointInPath(shape[0], newX, newY) && shape[1]) || (this.ctx.isPointInStroke(shape[0], newX, newY) && shape[2])) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  goto(x=0, y=0) {
-    this.x = x;
-    this.y = y;
-  }
-  changeby(dx=0, dy=0) {
-    this.x += dx;
-    this.y += dy;
-  }
-  point(deg=0) {
-    this.rotation = (deg + 180) % 360 - 180;
-  }
-  rotate(deg=0) {
-    this.point(this.rotation + deg);
-  }
-  setsize(size=1) {
-    this.scale = size;
-  }
-  changesize(ds=0) {
-    this.scale += ds;
-  }
-}
 
 let mouseX = 0;
 let mouseY = 0;
+window.addEventListener("mousemove", (event) => {
+  const bounds = game.canvas.getBoundingClientRect();
+  mouseX = event.clientX - bounds.left;
+  mouseY = event.clientY - bounds.top;
+});
 
 game.setupCanvas();
-ctx = game.ctx;
+const ctx = game.ctx;
 
 const box = new Sprite();
+box.goto(400, 400);
 game.addSprite(box);
-text = document.getElementById("text");
-i = 10;
-setTimeout(function move() {
-  box.goto(i/3, i/3);
-  box.point(i/2);
-  box.changesize(0.005);
-  if (box.pointInSprite(400, 400)) {
+let text = document.getElementById("text");
+let i = 10;
+setTimeout(function update() {
+  if (box.pointInSprite(mouseX, mouseY)) {
     ctx.fillStyle = "red";
   }
   else {
     ctx.fillStyle = "black";
   }
   game.clearCanvas();
-  ctx.beginPath();
-  ctx.setTransform(1, 0, 0, 1, 0, 0);
-  ctx.arc(400, 400, 4, 0, 2 * Math.PI);
-  fill = ctx.fillStyle;
-  ctx.fillStyle = "green";
-  ctx.fill();
-  ctx.fillStyle = fill;
   game.renderSprites();
   if (true) {
-    i += 1;
-    setTimeout(move, 10);
+    setTimeout(update, 10);
   }
-}, 100);
-
-window.addEventListener("mousemove", (event) => {
-  const bounds = game.canvas.getBoundingClientRect();
-  mouseX = event.clientX - bounds.left;
-  mouseY = event.clientY - bounds.top;
-});
+}, 10);

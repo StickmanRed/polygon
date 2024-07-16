7d92b92e0a2c3158c10af8d037054413e2e07090
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-07-15 19:48:15 -0700

    Update game.js

--- game.js
+++ game.js
@@ -117,7 +117,8 @@ ctx = game.ctx;
 const box = new Sprite();
 game.addSprite(box);
 // box.point(45);
-box.goto(400, 400)
+box.goto(400, 400);
+box.size(2);
 text = document.getElementById("text");
 i = 10;
 setTimeout(function move() {

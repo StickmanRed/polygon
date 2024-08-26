8db1cb92ea94b5866d81326414addc5106581ba6
StickmanRed <100049599+StickmanRed@users.noreply.github.com>
2024-08-25 21:14:39 -0700

    Replace pointInSprite logic with DOMMatrix
    Even though I like the inverse matrix solution, I'll replace it to simplify things.

--- sprite.js
+++ sprite.js
@@ -1,16 +1,3 @@
-/* The transformation matrix for scaleX, skewY, skewX, scaleY, shiftX, shiftY:
- * [ scaleX skewX  shiftX ]
- * [ skewY  scaleY shiftY ]
- * [ 0      0      1      ]
- */
-
-// Huge thanks to whoever wrote this! https://stackoverflow.com/a/60248778
-function transformMatrix(dx, dy, scale, rotate) {
-  const xAX = Math.cos(rotate) * scale;
-  const xAY = Math.sin(rotate) * scale;
-  return [xAX, xAY, -xAY, xAX, dx, dy];
-}
-
 export default class Sprite {
   constructor(x=0, y=0, scale=1, rotation=0, shown=true) {
     this.x = x;
@@ -30,17 +17,14 @@ export default class Sprite {
   }
 
   getTransform(x=this.x, y=this.y, scale=this.scale, rotation=this.rotation) {
-    return transformMatrix(x, y, scale, rotation * Math.PI / 180);
+    return new DOMMatrix([x, y, scale, rotation * Math.PI / 180]);
   }
 
   // Applies the inverse transformation to the point, then checks if one of the original shapes contains this point
   pointInSprite(x=0, y=0) {
-    const transformValues = this.getTransform();
-    const matrix = math.matrix([[transformValues[0], transformValues[2], transformValues[4]], [transformValues[1], transformValues[3], transformValues[5]], [0, 0, 1]]);
-    const inverseMatrix = math.inv(matrix);
-    let newX = inverseMatrix.get([0,0]) * x + inverseMatrix.get([0,1]) * y + inverseMatrix.get([0,2]);
-    let newY = inverseMatrix.get([1,0]) * x + inverseMatrix.get([1,1]) * y + inverseMatrix.get([1,2]);
-    return (this.ctx.isPointInPath(this.shapes[this.costume][0], newX, newY) && this.shapes[this.costume][1]) || (this.ctx.isPointInStroke(this.shapes[this.costume][0], newX, newY) && this.shapes[this.costume][2]);
+    const transformedPath = new Path2D();
+    transformedPath.addPath(this.shapes[this.costume], this.getTransform());
+    return this.ctx.isPointInPath(transformedPath, x, y) && (this.shapes[this.costume][1] || this.shapes[this.costume][2]);
   }
 
   goto(x=0, y=0) {
